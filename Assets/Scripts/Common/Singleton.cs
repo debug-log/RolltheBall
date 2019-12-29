a1ace810339e052f84314ba974d6029d7094803f
sea2003 <sea900318@gmail.com>
2019-12-29 15:21:07 +0900

    - 프로젝트 세팅

---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static bool _shuttingDown = false;
+    public static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(_shuttingDown)
+            {
+                Debug.LogWarning ("[Singleton] Instance '" + typeof (T) + "' already destroyed. Returning null.");
+                return null;
+            }
+
+            if(_instance == null)
+            {
+                var singletonObject = new GameObject ();
+                _instance = singletonObject.AddComponent<T> ();
+                singletonObject.name = typeof (T).ToString () + " (Singleton)";
+
+                DontDestroyOnLoad (singletonObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    private void OnApplicationQuit ()
+    {
+        _shuttingDown = true;
+    }
+
+    private void OnDestroy ()
+    {
+        _shuttingDown = true;
+    }
+}
